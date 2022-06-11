9de56fc38427eae3dfc6c28d612413f5182892a2
kacperpucylo <pucylek@wp.pl>
2022-06-11 20:01:43 +0200

    pair and make_pair implemented

--- utils.hpp
+++ utils.hpp
@@ -290,6 +290,81 @@ namespace ft
 		}
 		return (first2 != last2);
 	}
+
+	//holds a pair of values
+	template <class T1, class T2>
+	class pair
+	{
+	public:
+
+		typedef T1 first_type;
+		typedef T2 second_type;
+
+		pair(void) : first(), second() {}
+		//init with given values
+		pair(const first_type &a, const second_type &b) : first(a), second(b) {}
+
+		template <class U, class V>
+		pair(const pair<U, V> &pr) : first(pr.first), second(pr.second) {}
+
+		~pair(void) {}
+
+		const pair &operator=(const pair &rhs)
+		{
+			this->first = rhs.first;
+			this->second = rhs.second;
+			return (*this);
+		}
+
+		first_type first;
+		second_type second;
+
+	private:
+	};
+
+	//logical operators for ft::pair
+	template <class T1, class T2>
+	bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return (lhs.first == rhs.first && lhs.second == rhs.second);
+	}
+
+	template <class T1, class T2>
+	bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return (!(lhs == rhs));
+	}
+
+	template <class T1, class T2>
+	bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return (lhs.first < rhs.first || lhs.second < rhs.second);
+	}
+
+	template <class T1, class T2>
+	bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return (!(rhs < lhs));
+	}
+
+	template <class T1, class T2>
+	bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return (rhs < lhs);
+	}
+
+	template <class T1, class T2>
+	bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return (!(lhs < rhs));
+	}
+
+	//constructs and returns a pair of x and y values
+	template <class T1, class T2>
+	pair<T1, T2> make_pair(T1 x, T2 y)
+	{
+		return (pair<T1, T2>(x, y));
+	}
 }
 
 #endif
